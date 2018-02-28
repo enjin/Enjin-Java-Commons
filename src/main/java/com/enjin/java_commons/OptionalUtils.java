de6a27b601b083e06e76873ce31e5a856344c687
Evan Lindsay <evanlindsay@live.com>
2018-02-28 05:41:33 -0800

    Add helper methods for String, Map, and Collection to OptionalUtils

--- src/main/java/com/enjin/java_commons/OptionalUtils.java
+++ src/main/java/com/enjin/java_commons/OptionalUtils.java
@@ -1,5 +1,7 @@
 package com.enjin.java_commons;
 
+import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
 
 /**
@@ -36,6 +38,45 @@ public class OptionalUtils {
      */
     public static boolean isPresent(final Optional<?> optional) {
         return !ObjectUtils.isNull(optional) && optional.isPresent();
+    }
+
+    /**
+     * <p>Checks if a string is present in an {@link Optional} and not empty.</p>
+     *
+     * @param optional the optional to test
+     *
+     * @return - boolean {@code true} if optional is not {@code null}, a string is present and not empty.
+     *
+     * @since 1.0
+     */
+    public static boolean isStringPresent(final Optional<String> optional) {
+        return isPresent(optional) && !optional.get().isEmpty();
+    }
+
+    /**
+     * <p>Checks if a map is present in an {@link Optional} and not empty.</p>
+     *
+     * @param optional the optional to test
+     *
+     * @return - boolean {@code true} if optional is not {@code null}, a map is present and not empty.
+     *
+     * @since 1.0
+     */
+    public static boolean isMapPresent(final Optional<Map> optional) {
+        return isPresent(optional) && !optional.get().isEmpty();
+    }
+
+    /**
+     * <p>Checks if a collection is present in an {@link Optional} and not empty.</p>
+     *
+     * @param optional the optional to test
+     *
+     * @return - boolean {@code true} if optional is not {@code null}, a collection is present and not empty.
+     *
+     * @since 1.0
+     */
+    public static boolean isCollectionPresent(final Optional<Collection> optional) {
+        return isPresent(optional) && !optional.get().isEmpty();
     }
 
 }
